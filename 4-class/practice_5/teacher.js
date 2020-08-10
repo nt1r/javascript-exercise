764d10dd64bae1ae13a546776bf1ab8d0eb60123
leqi <michaelleqihust@gmail.com>
2020-08-10 16:54:01 +0800

    [leqi] feat: finish 4-class.

--- 4-class/practice_5/teacher.js
+++ 4-class/practice_5/teacher.js
@@ -1,1 +1,14 @@
-// TODO 20: 在这里写实现代码
+import Person from './person';
+
+export default class Teacher extends Person {
+  constructor(name, age, klass = null) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    return this.klass === null
+      ? `${super.introduce()} I am a Teacher. I teach No Class.`
+      : `${super.introduce()} I am a Teacher. I teach Class ${this.klass}.`;
+  }
+}
